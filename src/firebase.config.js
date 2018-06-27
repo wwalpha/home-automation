b4dcca36794fc32cb91901c943f57a2f95c6e9a0
陶 金 <kinto@dcs.co.jp>
2018-06-27 17:45:28 +0900

    add new project

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: 'XXXXXXXXXXXXXXXX-XXXXXXXXXXXX',
+  authDomain: 'プロジェクト ID.firebaseapp.com',
+  databaseURL: 'https://プロジェクト ID.firebaseio.com',
+  storageBucket: 'プロジェクト ID.appspot.com',
+  messagingSenderId: '0123456789',
+};
+
+module.exports = config;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+const firebase = require('firebase');
+const config = require('./firebase.config');
+
+const app = firebase.initializeApp(config, 'name');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.addScope('https://www.googleapis.com/auth/plus.login');
+
+app.auth().signInWithPopup(provider).then((result) => {
+  console.log(result);
+
+  // const token = result.credential.accessToken;
+}).catch(err => console.error(err));
+
+
+module.exports = app;
