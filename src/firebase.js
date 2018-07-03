deb4b00ab2aae49f4e3569715347946f0fd52181
wwalpha <wwalpha@gmail.com>
2018-07-03 22:59:24 +0900

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
-const firebase = require('firebase');
-const config = require('./firebase.config');
+// const firebase = require('firebase');
+// const config = require('./firebase.config');
 
-const app = firebase.initializeApp(config, 'name');
+// const app = firebase.initializeApp(config, 'name');
 
-const provider = new firebase.auth.GoogleAuthProvider();
+// const provider = new firebase.auth.GoogleAuthProvider();
 
-provider.addScope('https://www.googleapis.com/auth/plus.login');
+// provider.addScope('https://www.googleapis.com/auth/plus.login');
 
-app.auth().signInWithPopup(provider).then((result) => {
-  console.log(result);
+// app.auth().signInWithPopup(provider).then((result) => {
+//   console.log(result);
 
-  // const token = result.credential.accessToken;
-}).catch(err => console.error(err));
+//   // const token = result.credential.accessToken;
+// }).catch(err => console.error(err));
 
-app.database.ref('/').on('value', (snapshot) => {
-  console.log(snapshot);
-});
+// app.database.ref('/').on('value', (snapshot) => {
+//   console.log(snapshot);
+// });
 
-module.exports = app;
+// module.exports = app;
